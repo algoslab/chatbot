8c8ffa07909055459a9a6a4d2124f19060b71c38
Kamrul Hasan <com.algoslab@gmail.com>
2026-07-09 16:13:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import express from 'express';
+
+const route = express();
+dotenv.config();
+
+const host = process.env.OLLAMA_HOST;
+const port = process.env.OLLAMA_PORT;
+
+//express.static('ebot');
+route.use(express.static('views'));
+
+route.get('/', (req, res) => {
+    res.sendFile('index.html');
+})
+
+route.listen(process.env.SERVER_PORT, () => {
+    console.log(`Listening on port: ${process.env.SERVER_PORT}`);
+});
