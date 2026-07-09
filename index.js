076b6fd1c3b79267f1f2bf41aa054960cee18136
Kamrul Hasan <com.algoslab@gmail.com>
2026-07-09 16:25:22 +0600

    style of console.

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ route.get('/', (req, res) => {
 })
 
 route.listen(process.env.SERVER_PORT, () => {
-    console.log(`Listening on port: ${process.env.SERVER_PORT}`);
+    console.log(`Listening on: ${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`);
 });
